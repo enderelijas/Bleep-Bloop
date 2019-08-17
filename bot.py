2f6f34ec68c04b6559a4c5f6cf4e87a25b5164b8
enderelijas <50271707+enderelijas@users.noreply.github.com>
2019-08-17 13:01:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import random
 
 
-bot = commands.Bot ("?")
+bot = commands.Bot ("c.")
 
 @bot.event
 async def on_ready():
@@ -15,9 +15,4 @@ async def ping(ctx):
     await ctx.send("Pong :stuck_out_tongue_closed_eyes: ")
 
 
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title="EnderBot",description="Here To make this server a better place!")
-
-
-bot.run("NTY1OTUxNDYyNDkzMTI2NjY3.XOmMJA.a4WTS6oh34ppT29-JGtARcfmQM4")
+bot.run(os.getenv('TOKEN')) 
